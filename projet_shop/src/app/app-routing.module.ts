5d5f5afe01015d0b47d941973ee41db64e2091ed
Nicolas MOUTON--BESSON <nicolas.moutonbesson@gmail.com>
2023-02-15 14:59:45 +0100

    finish

--- projet_shop/src/app/app-routing.module.ts
+++ projet_shop/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from 'src/accueil/components/accueil/accueil.component';
 import { CategoryDisplayComponent } from '../catalog/components/category-display/category-display.component';
+import { CartDisplayComponent } from '../cart/components/cart-display/cart-display.component';
 
 const routes: Routes = [
   { path: '', component: AccueilComponent },
   { path: 'category/:id', component: CategoryDisplayComponent },
+  { path: 'cart', component: CartDisplayComponent },
 ];
 
 @NgModule({
